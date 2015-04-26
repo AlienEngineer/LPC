36312bdb15c39dc2a495b044b25e7294e13e8ff6
AlienEngineer <Aimirim.Software@gmail.com>
2015-04-26 18:57:27 +0100

    startup with GPIO

---
+++ GPIO/inc/GPIO.h
@@ -0,0 +1,17 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: 26/04/2015
+ *      Author: Alien
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+class GPIO {
+public:
+	GPIO();
+	virtual ~GPIO();
+};
+
+#endif /* GPIO_H_ */
---
+++ GPIO/src/GPIO.cpp
@@ -0,0 +1,18 @@
+/*
+ * GPIO.cpp
+ *
+ *  Created on: 26/04/2015
+ *      Author: Alien
+ */
+
+#include "GPIO.h"
+
+GPIO::GPIO() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GPIO::~GPIO() {
+	// TODO Auto-generated destructor stub
+}
+
