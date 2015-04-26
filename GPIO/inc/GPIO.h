8791efefed1e4df6ddf7459e0c5bf323bd739b6a
AlienEngineer <Aimirim.Software@gmail.com>
2015-04-26 21:33:40 +0100

    GPIO and Tests project

--- GPIO/inc/GPIO.h
+++ GPIO/inc/GPIO.h
@@ -8,10 +8,28 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+#include "LPC17xx.h"
+#include "core_cm3.h"
+#include "Common.h"
+
 class GPIO {
 public:
-	GPIO();
+	GPIO(LPC_GPIO_TypeDef * port);
 	virtual ~GPIO();
+
+	/**
+	 * Set the pin mode for the given pin number.
+	 * @param direction
+	 * @param pin
+	 */
+	__INLINE void PinMode(uint32_t pin, uint32_t direction);
+
+	uint32_t DigitalRead(uint32_t pin);
+	__INLINE void DigitalWrite(uint32_t pin, uint32_t value);
+
+private:
+	LPC_GPIO_TypeDef * GPIOPx;
+
 };
 
 #endif /* GPIO_H_ */
--- GPIO/src/GPIO.cpp
+++ GPIO/src/GPIO.cpp
@@ -7,12 +7,30 @@
 
 #include "GPIO.h"
 
-GPIO::GPIO() {
-	// TODO Auto-generated constructor stub
-
+GPIO::GPIO(LPC_GPIO_TypeDef * port) {
+	this->GPIOPx = port;
 }
 
 GPIO::~GPIO() {
-	// TODO Auto-generated destructor stub
+
 }
 
+__INLINE void GPIO::PinMode(uint32_t pin, uint32_t direction)
+{
+	this->GPIOPx->FIODIR = (direction == INPUT) ?
+			this->GPIOPx->FIODIR & ~pin :
+			this->GPIOPx->FIODIR | pin;
+}
+
+void GPIO::DigitalWrite(uint32_t pin, uint32_t value) {
+	if (value == HIGH) {
+		this->GPIOPx->FIOSET = pin;
+	}
+	else {
+		this->GPIOPx->FIOCLR |= pin;
+	}
+}
+
+__INLINE uint32_t GPIO::DigitalRead(uint32_t pin) {
+	return (this->GPIOPx->FIOPIN & pin) ? HIGH : LOW;
+}
