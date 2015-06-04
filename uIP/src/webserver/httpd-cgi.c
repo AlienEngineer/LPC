141ffdfec468c2c809bfc9d53cbb1dc2a87753dd
AlienEngineer <aimirim.software@gmail.com>
2015-06-04 20:08:09 +0100

    created new layout

--- uIP/src/webserver/httpd-cgi.c
+++ uIP/src/webserver/httpd-cgi.c
@@ -179,8 +179,7 @@ static unsigned short
 generate_net_stats(void *arg)
 {
   struct httpd_state *s = (struct httpd_state *)arg;
-  return snprintf((char *)uip_appdata, UIP_APPDATA_SIZE,
-		  "%5u\n", ((uip_stats_t *)&uip_stat)[s->count]);
+  return snprintf((char *)uip_appdata, UIP_APPDATA_SIZE, "%5u\n", ((uip_stats_t *)&uip_stat)[s->count]);
 }
 
 static
@@ -190,8 +189,7 @@ PT_THREAD(net_stats(struct httpd_state *s, char *ptr))
 
 #if UIP_STATISTICS
 
-  for(s->count = 0; s->count < sizeof(uip_stat) / sizeof(uip_stats_t);
-      ++s->count) {
+  for(s->count = 0; s->count < sizeof(uip_stat) / sizeof(uip_stats_t); ++s->count) {
     PSOCK_GENERATOR_SEND(&s->sout, generate_net_stats, s);
   }
   
