201b0f190e2f294bfd6819e0fc34d184fc932416
AlienEngineer <aimirim.software@gmail.com>
2015-06-10 09:26:01 +0100

    creating a simple context of execution

---
+++ WebServer/inc/Context.h
@@ -0,0 +1,18 @@
+/*
+ * Context.h
+ *
+ *  Created on: 09/06/2015
+ *      Author: sergio.ferreira
+ */
+
+#ifndef CONTEXT_H_
+#define CONTEXT_H_
+
+class Context {
+public:
+	uint8_t CurrentTemperature;
+	int8_t LimitInf;
+	int8_t LimitSup;
+};
+
+#endif /* CONTEXT_H_ */
